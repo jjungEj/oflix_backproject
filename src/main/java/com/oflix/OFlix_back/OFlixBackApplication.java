4e3ee2dd5c04c0de03518c7bea4dfeb136a7abc8
이지민 <wlals5213@naver.com>
2025-02-05 17:47:38 +0900

    init

---
+++ src/main/java/com/oflix/OFlix_back/OFlixBackApplication.java
@@ -0,0 +1,13 @@
+package com.oflix.OFlix_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OFlixBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OFlixBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oflix/OFlix_back/OFlixBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oflix.OFlix_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OFlixBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
