1c113ee9bcd686c39660d03de5ea9e5928a647f8
pejj212 <pejj212@gmail.com>
2025-02-14 02:34:27 +0900

    feat : movie dto 수정

--- src/main/java/com/oflix/OFlix_back/movie/dto/ResponseMovieDto.java
+++ src/main/java/com/oflix/OFlix_back/movie/dto/ResponseMovieDto.java
@@ -1,10 +1,15 @@
 package com.oflix.OFlix_back.movie.dto;
 
+import com.oflix.OFlix_back.image.dto.ResponseImageDto;
+import com.oflix.OFlix_back.image.entity.Image;
 import com.oflix.OFlix_back.movie.entity.Genre;
 import com.oflix.OFlix_back.movie.entity.Movie;
 import com.oflix.OFlix_back.movie.entity.Nation;
 import com.oflix.OFlix_back.movie.entity.ViewAge;
 import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -25,6 +30,19 @@ public class ResponseMovieDto {
     private Nation nation;
     private ViewAge viewAge;
 
+    //순환참조 오류를 막기 위해 이미지의 dto를 받아옴
+    private List<ResponseImageDto> images;
+
     public ResponseMovieDto(Movie movie) {
+        this.movieId = movie.getMovieId();
+        this.title = movie.getTitle();
+        this.releaseDate = movie.getReleaseDate();
+        this.director = movie.getDirector();
+        this.actors = movie.getActors();
+        this.synopsis = movie.getSynopsis();
+        this.genre = movie.getGenre();
+        this.nation = movie.getNation();
+        this.viewAge = movie.getViewAge();
+        this.images = movie.getImages().stream().map(ResponseImageDto::new).collect(Collectors.toList());
     }
 }
