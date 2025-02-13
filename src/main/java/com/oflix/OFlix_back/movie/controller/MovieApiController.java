d1f41981c95910d7f586bd05dd962c0062eed3b7
pejj212 <pejj212@gmail.com>
2025-02-13 15:49:01 +0900

    feat : 영화 추가 컨트롤러 로직 구현

--- src/main/java/com/oflix/OFlix_back/movie/controller/MovieApiController.java
+++ src/main/java/com/oflix/OFlix_back/movie/controller/MovieApiController.java
@@ -1,5 +1,8 @@
 package com.oflix.OFlix_back.movie.controller;
 
+import com.oflix.OFlix_back.image.dto.RequestMainPosterDto;
+import com.oflix.OFlix_back.image.dto.RequestStillCutsDto;
+import com.oflix.OFlix_back.movie.dto.TotalResponseMovieDto;
 import org.springframework.ui.Model;
 import com.oflix.OFlix_back.movie.dto.RequestMovieDto;
 import com.oflix.OFlix_back.movie.dto.ResponseMovieDto;
@@ -12,6 +15,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -35,11 +39,15 @@ public class MovieApiController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    //영화 추가
     @PostMapping
-    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
-        Movie createdMovie = movieService.createMovie(movie);
+    public ResponseEntity<TotalResponseMovieDto> createMovie(@RequestBody RequestMovieDto requestMovieDto, @RequestPart RequestMainPosterDto mainPosterDto, @RequestPart RequestStillCutsDto stillCutsDto) {
+        TotalResponseMovieDto createdMovie = movieService.createMovie(requestMovieDto, mainPosterDto, stillCutsDto);
         return new ResponseEntity<>(createdMovie, HttpStatus.CREATED);
     }
+
+
     @PutMapping("/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody RequestMovieDto requestMovieDto) {
         Movie updatedMovie = movieService.updateMovie(id, requestMovieDto);
