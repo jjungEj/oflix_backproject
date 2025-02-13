190262fc5ac7e26fa6ca11bde4bd501ee6b31ed2
pejj212 <pejj212@gmail.com>
2025-02-13 14:40:38 +0900

    feat : 메인포스터 업로드, 스틸컷이미지 업로드 로직 분리

--- src/main/java/com/oflix/OFlix_back/image/service/ImageService.java
+++ src/main/java/com/oflix/OFlix_back/image/service/ImageService.java
@@ -1,38 +1,81 @@
 package com.oflix.OFlix_back.image.service;
 
 import com.oflix.OFlix_back.image.entity.Image;
+import com.oflix.OFlix_back.image.entity.ImageType;
 import com.oflix.OFlix_back.image.repository.ImageRepository;
+import com.oflix.OFlix_back.movie.entity.Movie;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ImageService {
     private final ImageRepository imageRepository;
     private final ImageS3Service imageS3Service;
 
-    //이미지 업로드
-    //여기서 파일명하고 경로를 저장함
-    public String uploadImage(MultipartFile file){
+    //메인이미지랑 스틸컷 이미지 구현 로직을 나누는게 나을지도
+    //메인이미지 업로드
+    public String uploadMainImage(MultipartFile file, Movie movie){
         //이미지를 s3에 저장하고 경로를 반환받음
-        String url = imageS3Service.uploadS3Image(file);
-
+        String imagePath = imageS3Service.uploadS3Image(file);
         //반환받은 경로에서 파일명을 잘라냄
-        String fileName = getFileName(url);
+        String fileName = getFileName(imagePath);
         //이미지 객체에 저장
-        Image image = new Image();
-        //경로 저장
-        image.setImagePath(url);
-        //파일명 저장
-        image.setImageName(fileName);
-        //이미지 구분...어떻게 해야할지 모르겠음
-
+        Image main = Image.builder()
+                .imagePath(imagePath)
+                .imageName(fileName)
+                .imageType(ImageType.MAIN)
+                .movie(movie)
+                .build();
         //DB에 저장
-        //imageRepository.save(image);
+        imageRepository.save(main);
 
-        return url;
+        return imagePath;
+    }
+
+    //스틸컷들 업로드
+    public List<String> uplpadStillCuts(List<MultipartFile> files, Movie movie){
+        //반복되는 파일 경로와 이름을 받을 리스트
+        List<String> path = new ArrayList<>();
+        List<String> filenames = new ArrayList<>();
+
+        //for문으로 이미지를 S3에 업로드하고 리스트에 경로와 이름을 저장
+        for(MultipartFile file : files){
+            //s3에 이미지 업로드하고 경로, 파일명 반환
+            String url = imageS3Service.uploadS3Image(file);
+            String fileName = getFileName(url);
+            //리스트에 추가
+            path.add(url);
+            filenames.add(fileName);
+            log.info(url);
+            log.info(fileName);
+        }
+
+        log.info(String.valueOf(path.size()));
+        log.info(String.valueOf(filenames.size()));
+
+        for(int i=0; i<path.size(); i++){
+            //이미지 객체 생성
+            Image stilCut = Image.builder()
+                    .imagePath(path.get(i))
+                    .imageName(filenames.get(i))
+                    .imageType(ImageType.STILL)
+                    .movie(movie)
+                    .build();
+            //DB에 저장
+            imageRepository.save(stilCut);
+        }
+
+        return path;
     }
 
     //경로에서 파일명을 추출하는 메서드
@@ -56,3 +99,28 @@ public class ImageService {
         imageRepository.deleteById(imageId);
     }
 }
+
+
+    /*
+    //이미지 업로드
+    //여기서 파일명하고 경로를 저장함
+    public String uploadImage(MultipartFile file){
+        //이미지를 s3에 저장하고 경로를 반환받음
+        String url = imageS3Service.uploadS3Image(file);
+
+        //반환받은 경로에서 파일명을 잘라냄
+        String fileName = getFileName(url);
+        //이미지 객체에 저장
+        Image image = new Image();
+        //경로 저장
+        image.setImagePath(url);
+        //파일명 저장
+        image.setImageName(fileName);
+        //이미지 구분...어떻게 해야할지 모르겠음
+
+        //DB에 저장
+        //imageRepository.save(image);
+
+        return url;
+    }
+    */
