6860ef51e496c266d1245d28c2cb11ba1e6df2e8
jungjung <dmswn3649@naver.com>
2025-02-19 15:44:51 +0900

    feat:movie

---
+++ src/main/java/com/oflix/OFlix_back/admin/dto/ResponseAdminDto.java
@@ -0,0 +1,4 @@
+package com.oflix.OFlix_back.admin.dto;
+
+public class ResponseAdminDto {
+}
---
+++ src/main/java/com/oflix/OFlix_back/admin/repository/AdminRepository.java
@@ -0,0 +1,4 @@
+package com.oflix.OFlix_back.admin.repository;
+
+public interface AdminRepository {
+}
