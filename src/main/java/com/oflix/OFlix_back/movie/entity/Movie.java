2b525f1f154747ef3509ad14228f48166a7a5fe3
jungjung <dmswn3649@naver.com>
2025-02-21 18:09:33 +0900

    merge 덜 된 부분

--- src/main/java/com/oflix/OFlix_back/movie/entity/Movie.java
+++ src/main/java/com/oflix/OFlix_back/movie/entity/Movie.java
@@ -86,4 +86,12 @@ public class Movie {
                 .movieStatus(movieStatus)
                 .build();
     }
+    public String addImage(Image image) {
+        if(this.images==null){
+            this.images = new ArrayList<>();
+        }
+        images.add(image);
+        image.setMovie(this);
+        return images.toString();
+    }
 }
